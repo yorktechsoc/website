c0c88c9cbc57c71a8103ef20d75228224077ccac
cherry-john <cherry-john@users.noreply.github.com>
2023-02-09 11:29:08 +0000

    Remove preview for PR 7 🛬

--- pr-preview/pr-7/assets/js/69369ae2.4a0f7f92.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktechsoc_website=self.webpackChunktechsoc_website||[]).push([[62],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- pr-preview/pr-7/assets/js/c4348237.8f68b1c3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktechsoc_website=self.webpackChunktechsoc_website||[]).push([[277],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
